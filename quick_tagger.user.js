19b851821dd53b50a2d298e50b52ca826d5d4033
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-06-19 20:00:45 -0400

    Updated header info

--- quick_tagger.user.js
+++ quick_tagger.user.js
@@ -1,18 +1,24 @@
 // ==UserScript==
 // @name         e621 quick tagger
-// @description  Custom page for quickly tagging sets of tags
+// @description  Custom page for quickly and effectively working on tagging projects
 
-// @namespace   http://tampermonkey.net/
-// @version     1.00005
+// @namespace    https://github.com/sasquire
+// @author       Sasquire
+// @version      1.00005
+// @updateURL    https://github.com/Sasquire/quick_tagger/raw/master/source/header.js
+// @downloadURL  https://github.com/Sasquire/quick_tagger/raw/master/quick_tagger.user.js
 
-// @author      Sasquire
+// @homepage     https://github.com/Sasquire/quick_tagger
+// @source       https://github.com/Sasquire/quick_tagger
+// @supportURL   https://e621.net/user/show/170289
+// @icon
 
-// @match       http://e621.net/extensions/quick_tagger
-// @match       https://e621.net/extensions/quick_tagger
+// @match        http://e621.net/extensions/quick_tagger
+// @match        https://e621.net/extensions/quick_tagger
 
-// @grant       GM_addStyle
-// @grant       GM.setValue
-// @grant       GM.getValue
+// @grant        GM_addStyle
+// @grant        GM.setValue
+// @grant        GM.getValue
 // ==/UserScript==
 
 // Init is run at bottom of script
